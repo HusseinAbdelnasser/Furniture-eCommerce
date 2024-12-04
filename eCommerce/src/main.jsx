0d94e9c762befeff2f1ae48d2f18ddc7690937f6
Hussein Abdelnasser <husseinkhaled9847@gmail.com>
2024-12-04 20:11:27 +0200

    update

--- eCommerce/src/main.jsx
+++ eCommerce/src/main.jsx
@@ -4,23 +4,19 @@ import "remixicon/fonts/remixicon.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import App from "./App.jsx";
-import store from "./redux/store.js";
-import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-      <Provider store={store}>
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          closeOnClick
-          pauseOnHover={false}
-          theme="light"
-        />
-        <ToastContainer />
-        <App />
-      </Provider>
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      closeOnClick
+      pauseOnHover={false}
+      theme="light"
+    />
+    <ToastContainer />
+    <App />
   </StrictMode>
 );
